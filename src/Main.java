be1efa5a9d78f92bc59d891046b3f8410fa31d36
Vasili <vasili.trutnev@gmail.com>
2023-04-05 22:24:46 +0500

    Сorrection

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Бонус");
-        int x = 50_000; // x - стоимость билета
-        int y = 20; // y - колличество рублей потраченных для начисления милль
-        int z = 1; // z - колличество миль начисляемых за каждые потраченные "y" рублей
-        int bonus = x / y * z; // bonus - сколько бонусов должен получить человек
-        System.out.println(bonus);
-        System.out.println("Рублей");
+        int price = 50_000; // Стоимость билета
+        int condition = 20; // Колличество рублей потраченных для начисления 1 милли
+        int miles = 1; // Колличество миль начисляемых за каждые потраченные 20 рублей
+        int bonus = price / condition * miles; // Сколько бонусов должен получить человек
+        System.out.println(bonus + " рублей");
     }
 }
